1d22a2e000e7bd442b009248f7f11e36ec6eb54f
Karl Nicholas <karl.nicholas@outlook.com>
2024-03-07 10:48:34 -0500

    updates

--- src/main/java/com/example/bff/BffApplication.java
+++ src/main/java/com/example/bff/BffApplication.java
@@ -2,12 +2,12 @@ package com.example.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 @RequestMapping
 public class BffApplication {
 
