7932bfba297dfd6623cf65539c68780502c4e277
Karl Nicholas <karl.nicholas@outlook.com>
2024-03-07 10:26:07 -0500

    muck with continuous deploy

--- src/main/java/com/example/bff/BffApplication.java
+++ src/main/java/com/example/bff/BffApplication.java
@@ -1,23 +1,23 @@
-package org.example.essentialcloud.bff;
+package com.example.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 @RequestMapping
 public class BffApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BffApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BffApplication.class, args);
+	}
 
-    @GetMapping
-    public String getHello() {
-        return "Hello World.";
-    }
+	@GetMapping
+	public String getHello() {
+		return "Hello World.";
+	}
 
 }
--- src/test/java/com/example/bff/BffApplicationTests.java
+++ src/test/java/com/example/bff/BffApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.essentialcloud.bff;
+package com.example.bff;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BffApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
