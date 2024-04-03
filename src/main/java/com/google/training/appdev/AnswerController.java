74254b8cc9ce05e282e832f098836805f21585f2
Karl Nicholas <karl.nicholas@outlook.com>
2024-04-03 18:10:32 -0400

    cleanup

--- src/main/java/com/google/training/appdev/AnswerController.java
+++ src/main/java/com/google/training/appdev/AnswerController.java
@@ -14,23 +14,6 @@ public class AnswerController {
     public String getAccounts(@RequestParam("uid") String uid) {
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
         return "{\"uid\": \"" + uid + "\", \"principal\": \"" + name + "\"}";
-//		try (LanguageServiceClient language = LanguageServiceClient.create()) {
-//			Document doc =
-//					Document.newBuilder().setContent("After a yearlong investigation, Hur concluded that Biden mishandled classified material and improperly disclosed classified information as a private citizen. However, Hur said that there wasn’t enough evidence to prosecute Biden, and that he wouldn’t have pursued charges even if the Justice Department allowed him to indict a sitting president. But it was Hur’s assessment of Biden’s cognitive abilities that sent shockwaves throughout Washington. He described Biden as a “well-meaning, elderly man with a poor memory” in explaining the uphill battle prosecutors might face when trying to convince a potential jury that Biden was a criminal. CNN reported Tuesday morning that Biden told jokes and unfurled lengthy detailed stories from his decadeslong political career as he parried questions from Hur and his investigators over two days last October, a transcript of the interview reviewed by news outlets shows. The president also displayed episodes of foggy memory, including one highlighted in Hur’s final report, in which the president appeared not to remember the year that his son Beau died.")
-//							.setType(Document.Type.PLAIN_TEXT).build();
-//			AnalyzeSentimentResponse response = language.analyzeSentiment(doc);
-//			Sentiment sentiment = response.getDocumentSentiment();
-//			if (sentiment == null) {
-//				System.out.println("No sentiment found");
-//			} else {
-//				System.out.printf("Sentiment magnitude : %.3f\n", sentiment.getMagnitude());
-//				System.out.printf("Sentiment score : %.3f\n", sentiment.getScore());
-//			}
-//			System.out.print("Sentiment = " + sentiment);
-//			return Float.toString(sentiment.getScore());
-//		} catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
     }
 
 }
--- src/main/resources/static/firebaseAnswer.js
+++ src/main/resources/static/firebaseAnswer.js
@@ -2,19 +2,14 @@ import { getAuth} from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-auth
 
 getAuth().onAuthStateChanged(function(user) {
     if (user) {
-        // User is signed in.
-        console.log("logged in");
         callAPI(user)
-    } else {
-        console.log("NOT logged in");
-        // No user is signed in.
     }
 });
 
 async function callAPI(user) {
     if (user) {
         const token = await user.getIdToken()
-        const res = await fetch("http://localhost:8080/api/answer?uid="+user.uid, {
+        const res = await fetch(window.location.origin + "/api/answer?uid="+user.uid, {
             headers: {authorization: `Bearer ${token}`}
         })
         const answer = await res.json();
